74940c1df939ee6885bc988a1858be78465b88de
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-10-06 14:55:16 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,16 +1,25 @@
-const accountId = 144553
-let accountEmail = "suraj@google.com"
-var accountPassword = "12345"
-accountCity = "Mumbai"
-let accountState = 
+const accountId = 1445533;
+let accountEmail = "suraj@google.com";
+var accountPassword = "12345";
+accountCity = "Mumbai";
+let accountState;
 
-// accountId = 2
+// accountId = 2; // accountId is defined in const and we can't change any value which is in const.
 
-/* Prefer not to use var - because of issue in block scope and function scope*/
+accountEmail = "sb@sb.com";
+accountPassword = "21212121";
+accountCity = "Thane";
 
-console.log(accountId);
-accountEmail = "sb@sb.com"
-accountPassword = "21212121"
-accountCity = "Thane"
+//console.log(accountId);
+
+/* Prefer not use var - bacause of issue in block scope and functional scope */
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+// console.log(typeof accountId);
+// console.log(typeof accountEmail);
+// console.log(typeof accountPassword);
+// console.log(typeof accountCity);
+// console.log(typeof accountState);
+
+// console.table([typeof accountId, typeof accountEmail, typeof accountPassword, typeof accountCity, typeof accountState]);
