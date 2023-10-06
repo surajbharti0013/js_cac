ec7e05f492a1d01b8932f54dbddeaa765a2c3925
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-10-06 14:56:20 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ); // we are using node.js not browser.
+
+console.log( 3 
+    +
+    3 ); // Should not write like this.Code readability should be high 
+
+console.log("suraj");
+
+let name = "suraj";
+let age = 30;
+let isLoggedIn = false;
+
+// number => range of number is 2 to the power 53
+// bigint => We use bigint when numbers of digit is long.(We don't use normally )
+// string => Text inside single/double quotes.
+// boolean => just true/false. Something is true or false.
+// null => Reference of empty variable(Let we make any app we ask temp of someplace and server have some problem so it will send null if it sends zero(0) then zero(0) is also a value thats why we use null). Its standalone value.
+// undefined => We have created some variable but we have not assigned its value.
+// symbol => uniqueness, to create two component with same name and same value.
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //objects
