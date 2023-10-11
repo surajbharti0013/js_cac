c25dc0daaa01c9c54470981662970fcaf767a799
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-10-11 15:23:46 +0000

    comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -19,7 +19,6 @@
 
 // Note:- The reason is that an equality check == and comparisions > < >= <= work differently.
 // Comparisions convert null to a number, treating it as 0. Thats why(4) null >= 0 is true and (1) null > 0 is false.
-
 // console.log(null > 0);
 // console.log(null < 0);
 // console.log(null == 0);
