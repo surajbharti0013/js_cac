19b8c3ba33439148b164b5765207876750e86028
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-10-12 15:53:43 +0000

    strings methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,66 @@
+// const name = "suraj";
+// const number = 1234;
+// const repoCount = 50;
+
+// console.log(name + repoCount + " value");
+// console.log(`Hello, my name is ${name} and my repo count is ${repoCount} and value is 30`); // It is Object literals using back-ticks here you just have to put placeholder and directly inject variables its new feature and in old you have to put quotes everywhere
+
+// const gameName = new String("surajsb");
+const gameName = new String("Website https//suraj.com/suraj%20bharti");
+const str2 = 'world';
+
+// console.log(gameName[0]);  // tells on 0th index character is
+// console.log(gameName.__proto__);// This tells us that it is empty object etc.
+// console.log(gameName.length); // tells total length of string includes zeros
+// console.log(gameName.anchor("https://www.google.com")); // puts anchor tags around given variable.
+// console.log(gameName.at(-1)); // tells the starting character is on what index. (if we specifies positive number then it checks from left and if negative then from right side)
+// console.log(gameName.big()); // Puts big tag around variable.
+// console.log(gameName.blink()); // Puts blink tag around variable.
+// console.log(gameName.bold()); // Puts bold tag around variable.
+// console.log(gameName.charAt(2)); // can ask what text is on what index/number. 
+// console.log(gameName.charCodeAt(2)); // specify uni-code of given index/number. 
+// console.log(gameName.codePointAt()); // returns non negative integer 
+// console.log(gameName.concat(" " + str2)); // it is used to join two string.
+// console.log(gameName.constructor());
+// console.log(gameName.endsWith("s")); // check whether string ends with that given character. 
+// console.log(gameName.fixed()); // Puts tt tag around variable.
+// console.log(gameName.fontcolor()); // fontcolor.
+// console.log(gameName.fontsize()); // fontsize.
+// console.log(gameName.includes('sj')); // Check whether given text/word present in that variable. 
+// console.log(gameName.indexOf('r')); // Check place/number of given text in that variable. 
+// // console.log(gameName.isWellFormed()); // fontsize.
+// console.log(gameName.italics()); // Puts i tag around variable.
+// console.log(gameName.lastIndexOf('j')); // Check place/number of given text in that variable. 
+// console.log(gameName.link()); // Puts a tag around variable for link.
+// console.log(gameName.localeCompare()); // 
+// console.log(gameName.match('sb')); // 
+// console.log(gameName.matchAll()); // 
+// console.log(gameName.normalize()); // 
+// console.log(gameName.padStart(1,"0")); // 
+// console.log(gameName.padEnd(1,"0")); //
+// console.log(gameName.repeat('s','k'));
+// console.log(gameName.replace('s','k')); // This function replace first occurance of given text/string from mention string in bracket you have to just mention (we have to replace from what -to what).
+// console.log(gameName.replaceAll('s','k'));// This function replace all occurance of given text/string from mention string in bracket you have to just mention (we have to replaceAll from what -to what).
+// console.log(gameName.search('raj')); // This function tell the position/place of given text.
+// console.log(gameName.slice(0,3));// Here in bracket we define we want text from - to(Means from char 0 to 3 etc. here we can give negative value.
+// console.log(gameName.small()); // Puts small tag around variable.
+// console.log(gameName.split(' ')); // Converts string to arrays list.
+// console.log(gameName.startsWith('Website')); // Checks whether string starts with ('Website') give ans in true or false.
+// console.log(gameName.strike());// Puts strike tag around variable for link.
+// console.log(gameName.sub());// Puts sub tag around variable for link.
+// console.log(gameName.substr(1,6));// substr() extracts a part of a string from 1 to 6.
+// console.log(gameName.substring(1,6));// substr() extracts a part of a string from 1 to 5.
+// console.log(gameName.sup());// Puts sup tag around variable for link.
+// console.log(gameName.toLocaleUpperCase());// converts string to uppercase character
+// console.log(gameName.toUpperCase()); // converts string to uppercase character
+// console.log(gameName.toLocaleLowerCase());// converts string to lowercase character
+// console.log(gameName.toLowerCase()); // converts string to lowercase character
+// console.log(gameName.toString()); // toString() returns the content of a string.
+// //console.log(gameName.toWellFormed());
+// console.log(gameName.trim());
+// const stringOne = "     suraj bharti     ";
+// console.log(stringOne.trim()); // This function(trim()) removes the extra space around character from start and from end not between to character.
+// console.log(stringOne.trimStart()); // This function(trim()) removes the extra space from start but not between character.
+// console.log(stringOne.trimEnd()); // This function(trim()) removes the extra space from end but not between character.
+// console.log(stringOne.trimLeft()); // This function(trim()) removes the extra space from start but not between character.
+// console.log(stringOne.trimRight()); // This function(trim()) removes the extra space from end but not between character
