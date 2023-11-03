ab54f17ba40f50fc28bb2e6d23ef679bb31bac32
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-11-03 07:10:36 +0000

    Method chaning

---
+++ 05_iterations/07_seven.js
@@ -0,0 +1,12 @@
+// method chaning:- Applying multiple functions/property to one variable.
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumbers.map( (num) => { return num + 10})
+
+const newNums = myNumbers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
