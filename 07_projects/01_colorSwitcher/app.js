b6a38cec416335ef43ca0bbf8a9511844fb860ee
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-11-15 07:46:11 +0000

    01_ColorSwitcher

---
+++ 07_projects/01_colorSwitcher/app.js
@@ -0,0 +1,48 @@
+let allButtons = document.querySelectorAll(".button");
+let body = document.querySelector("body");
+let headingText1 = document.querySelector("h1");
+let headingText2 = document.querySelector("h2");
+
+// allButtons.forEach(function (box) {
+//   // console.log(button);
+//   box.addEventListener("click", function (e) {
+//     // console.log(e);
+//     // console.log(e.target);
+//     if (e.target.id === "grey") body.style.backgroundColor = e.target.id;
+//     if (e.target.id === "white") body.style.backgroundColor = e.target.id;
+//     if (e.target.id === "yellow") body.style.backgroundColor = e.target.id;
+//     if (e.target.id === "blue") body.style.backgroundColor = e.target.id;
+//   });
+// });
+
+allButtons.forEach((box) => {
+  box.addEventListener("click", function (e) {
+    // console.log(e);
+    // console.log(e.target);
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+      headingText1.style.color = "white";
+      headingText2.style.color = "white";
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+      headingText1.style.color = "black";
+      headingText2.style.color = "black";
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+      headingText1.style.color = "black";
+      headingText2.style.color = "black";
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+      headingText1.style.color = "white";
+      headingText2.style.color = "white";
+    }
+    if (e.target.id === "purple") {
+      body.style.backgroundColor = e.target.id;
+      headingText1.style.color = "white";
+      headingText2.style.color = "white";
+    }
+  });
+});
